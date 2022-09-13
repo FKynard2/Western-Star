8473e49faec9c39eaca45336f529b5763536e732
fkynard2@gmail.com <fkynard2@gmail.com>
2022-09-13 01:03:27 -0400

    accordion added funtionality

--- assets/superstore.js
+++ assets/superstore.js
@@ -40784,5 +40784,27 @@ class QuantityInput extends HTMLElement {
 customElements.define('quantity-input', QuantityInput);
 
 
-
- 
+// $('.question').click(function(e) {
+// 	e.preventDefault();
+// 	var currentIsActive = $(this).hasClass('is-active');
+// 	$(this).parent('.faq').find('> *').removeClass('is-active');
+// 	if(currentIsActive != 1) {
+// 		$(this).addClass('is-active');
+// 		$(this).next('.answer').addClass('is-active');
+// 	}
+// });
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
